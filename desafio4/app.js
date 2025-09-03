42227d914529c0ee5ca9a4338402b4b6b460d272
Rramirezch <rodrigora05@yahoo.com>
2025-09-03 11:20:08 -0500

    se adiciona extra

--- desafio4/app.js
+++ desafio4/app.js
@@ -16,6 +16,24 @@ while (numero1 != adivinar || contador < 3) {
     alert("Lo siento, no adivinaste el nro");
 }
 
+//ahora generando el numero a adivinar de forma aleatoria
+let numeroAdivinar = Math.floor(Math.random() * 10) + 1;
+let intentos = 0;
+let maxIntentos = 3;
+let numeroUsuario = prompt("Adivina el número del 1 al 10");
+
+while (numeroUsuario != numeroAdivinar && intentos < maxIntentos) {
+    intentos++;
+    if (intentos == maxIntentos || numeroUsuario == numeroAdivinar) {
+        break;
+    }
+    numeroUsuario = prompt("Intenta de nuevo, te quedan " + (maxIntentos - intentos) + " intentos");
+}if (numeroUsuario == numeroAdivinar) {
+    alert("Felicidades, adivinaste el número:", numeroAdivinar);
+} else {
+    alert("Lo siento, no adivinaste el número. El número era " + numeroAdivinar);
+}   SVGAnimatedLengthLis        
+
 
 
   
