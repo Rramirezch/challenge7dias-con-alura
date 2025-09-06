e2da027520352eec5b71dde91ca8f6a220dc1066
Rramirezch <rodrigora05@yahoo.com>
2025-09-06 08:24:11 -0500

    Se adiciona desafio 6-

--- desafio5/app.js
+++ desafio5/app.js
@@ -21,24 +21,48 @@ if (categoria === "frutas") {
     congelados.push(alimento);
 }
 
-let otraCompra = prompt("¿Desea agregar otro alimento? (si/no):").toLowerCase();
-if (otraCompra !== "no" && otraCompra !== "si") {
-        prompt("Por favor ingrese si o no");
-} while (otraCompra == "si") {
-    alimento = prompt("Ingrese un alimento que necesita comprar:");
-    categoria = prompt("Ingrese la categoria del alimento (frutas, verduras, lacteos, congelados):");
-    if (categoria !== "frutas" && categoria !== "verduras" && categoria !== "lacteos" && categoria !== "congelados") {
-    categoria = prompt("Por favor ingrese una categoria valida");
-    } if (categoria === "frutas") {
-        frutas.push(alimento);
-    } else if (categoria === "verduras") {
-        verduras.push(alimento);
-    } else if (categoria === "lacteos") {
-        lacteos.push(alimento);
-    } else if (categoria === "congelados") {
-        congelados.push(alimento);
-    }otraCompra = prompt("¿Desea agregar otro alimento? (si/no):").toLowerCase();
-}if (otraCompra === "no"){
+let otraCompra = prompt("¿Desea agregar otro alimento? (si/no), o si desea escriba eliminar :").toLowerCase();
+if (otraCompra !== "no" && otraCompra !== "si" && otraCompra !== "eliminar") {
+        otraCompra = prompt("Por favor ingrese si , no o eliminar").toLowerCase();
+    } while (otraCompra == "si" || otraCompra == "eliminar") {
+        if (otraCompra === "si") {
+            alimento = prompt("Ingrese un alimento que necesita comprar:");
+            categoria = prompt("Ingrese la categoria del alimento (frutas, verduras, lacteos, congelados):");
+            if (categoria !== "frutas" && categoria !== "verduras" && categoria !== "lacteos" && categoria !== "congelados") {
+            categoria = prompt("Por favor ingrese una categoria valida");
+            } if (categoria === "frutas") {
+                frutas.push(alimento);
+            } else if (categoria === "verduras") {
+                verduras.push(alimento);
+            } else if (categoria === "lacteos") {
+                lacteos.push(alimento);
+            } else {(categoria === "congelados") 
+                congelados.push(alimento);
+            }
+         } otraCompra = prompt("¿Desea agregar otro alimento? (si/no), o si desea escriba eliminar:").toLowerCase();
+        if(otraCompra === "eliminar") {
+            alert("Lista de compras:\n\nFrutas: " + frutas.join(", ") + "\nVerduras: " + verduras.join(", ") + "\nLacteos: " + lacteos.join(", ") + "\nCongelados: " + congelados.join(", "));
+            let eliminarAlimento = prompt("Ingrese el alimento que desea eliminar:");
+            if (frutas.includes(eliminarAlimento)) {
+                frutas.splice(frutas.indexOf(eliminarAlimento), 1);
+                alert(" Ha sido eliminado: " + eliminarAlimento);
+            } else if (verduras.includes(eliminarAlimento)) {
+                verduras.splice(verduras.indexOf(eliminarAlimento), 1);
+                alert(" Ha sido eliminado: " + eliminarAlimento);
+            } else if (lacteos.includes(eliminarAlimento)) {
+                lacteos.splice(lacteos.indexOf(eliminarAlimento), 1);
+                alert(" Ha sido eliminado: " + eliminarAlimento);
+            } else if (congelados.includes(eliminarAlimento)) {
+                congelados.splice(congelados.indexOf(eliminarAlimento), 1);
+                alert(" Ha sido eliminado: " + eliminarAlimento);
+            } else {
+                alert("El alimento no se encuentra en la lista");
+            }
+        otraCompra = prompt("¿Desea agregar otro alimento? (si/no), o si desea escriba eliminar:").toLowerCase();
+        }
+    
+if (otraCompra === "no"){
     alert("Gracias por su compra");
     alert("Lista de compras:\n\nFrutas: " + frutas.join(", ") + "\nVerduras: " + verduras.join(", ") + "\nLacteos: " + lacteos.join(", ") + "\nCongelados: " + congelados.join(", "));
     }
+}
